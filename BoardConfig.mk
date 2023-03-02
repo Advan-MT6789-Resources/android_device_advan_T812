dd76545254dca81ec19f2c049f6c33e079281001
Woomymy <woomy@woomy.be>
2023-03-02 20:40:16 +0100

    T812: Fix recovery kernel modules instalation
    * BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD should contain the
      list of modules loaded with the recovery, but the modules should be
      installed using BOARD_VENDOR_RAMDISK_KERNEL_MODULES
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I72768f8325017e0f5b969a7eb03ccccbf4b7eae0
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,15 @@ TARGET_NO_KERNEL_OVERRIDE := true
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules/vendor_ramdisk/modules.load))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules/vendor_ramdisk/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
 
-BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules/vendor_ramdisk/modules.load.recovery))
-BOARD_RECOVERY_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules/vendor_ramdisk/, $(BOARD_RECOVERY_KERNEL_MODULES_LOAD))
+# Also add recovery modules to vendor ramdisk
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules/vendor_ramdisk/modules.load.recovery))
+RECOVERY_MODULES := $(addprefix $(KERNEL_PATH)/modules/vendor_ramdisk/, $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD))
+
+# Prevent duplicated entries (to solve duplicated build rules problem)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES) $(RECOVERY_MODULES))
+
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules/vendor_dlkm/modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules/vendor_dlkm/, $(BOARD_VENDOR_KERNEL_MODULES_LOAD))
 
 # Inherit from the proprietary version
 include vendor/advan/T812/BoardConfigVendor.mk
