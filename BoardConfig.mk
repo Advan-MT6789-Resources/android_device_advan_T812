5ead91632c9d99097b414c50869ba560b7d0626f
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-10-14 02:26:22 +0700

    T812: Add kernel flags
    * Kernel is built separatly and placed in device/advan/T812-kernel
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,18 @@
 # Inherit from mt6789-common
 include device/advan/mt6789-common/BoardConfigCommon.mk
 
+# Boot image
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
 # Display
 TARGET_SCREEN_DENSITY := 280
+
+# DTB
+BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
+
+# Kernel
+TARGET_NO_KERNEL_OVERRIDE := true
 
 # Inherit from the proprietary version
 include vendor/advan/T812/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -5,9 +5,14 @@
 #
 
 DEVICE_PATH := device/advan/T812
+KERNEL_PATH := $(DEVICE_PATH)-kernel
 
 # Inherit from mt6789-common
 $(call inherit-product, device/advan/mt6789-common/common.mk)
+
+# Kernel
+PRODUCT_COPY_FILES += \
+	$(KERNEL_PATH)/Image.gz:kernel
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
