0fb040b0e89ba63872eb1c756182e139c0dcccaa
SGCMarkus <markusornik@gmail.com>
2023-10-19 11:53:28 +0200

    T812: Add security.rkp-V3 dependency to keymint service
    The symbol _ZN4aidl7android8hardware8security7keymint29IRemotelyProvisionedComponent10descriptorE
    was moved from android.hardware.security.keymint-V2-ndk to android.hardware.security.rkp-V3-ndk.
    
    Change-Id: I7f3c40e3e8a2a848cdcb73e5232952f63681aa58

--- extract-files.py
+++ extract-files.py
@@ -23,7 +23,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint-service.trustkernel': blob_fixup()
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V4-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
-        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so'),
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/lib64/mt6789/libcam.utils.sensorprovider.so': blob_fixup()
         .add_needed('libshim_sensors.so'),
     (
