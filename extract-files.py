32f0819edcc4fe23e4dabafb2a0a85099042d0ef
Michael Bestas <mkbestas@lineageos.org>
2024-11-05 23:19:25 +0200

    T812: ELF checks are enabled by default
    Change-Id: I5740f4c8d359831b4cb794912c5d650f55c33f17

--- extract-files.py
+++ extract-files.py
@@ -34,7 +34,6 @@ module = ExtractUtilsModule(
     'advan',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
-    check_elf=True,
     add_firmware_proprietary_file=True,
 )
 
