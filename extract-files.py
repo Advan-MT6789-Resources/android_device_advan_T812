602769b31ea7822a8f0845a7e82fe4e642581e7f
nift4 <nift4@protonmail.com>
2024-12-29 16:16:47 +0100

    T812: Add missing dependencies to offending libraries
    * Some libraries depend on symbols from other libraries that aren't
      defined as NEEDED. Since checkelf will throw an error
      in this case, use patchelf to add the missing libraries to
      NEEDED.
    
    Change-Id: I30c7fb2237420dedc8e4966f9c57cf07d099ffa3

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,13 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/mt6789/libmtkcam_stdutils.so'
     ): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
+    (
+        'vendor/lib64/mt6789/lib3a.ae.stat.so',
+        'vendor/lib64/mt6789/lib3a.flash.so',
+        'vendor/lib64/mt6789/lib3a.sensors.color.so',
+        'vendor/lib64/mt6789/lib3a.sensors.flicker.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
