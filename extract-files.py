b1c1c7c2fe3835bee90efbde6491053db83886de
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-22 01:19:42 +0530

    T812: Use prebuilt keymint ndk backend
      * V4 was fine on a16 but not anymore ?
    
    Change-Id: I27688b670007d2cdc0778815144dc304f20cda04

--- extract-files.py
+++ extract-files.py
@@ -21,7 +21,7 @@ namespace_imports = [
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint-service.trustkernel': blob_fixup()
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V4-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V3-ndk-mtk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
